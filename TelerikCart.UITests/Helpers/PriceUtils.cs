0853502f0543f8c36ceb5a2eeb63a729566d5229
np-allane <nikolay.prosenikov.extern@allane.com>
2024-11-09 21:48:34 +0200

    utils method for price parsing

---
+++ TelerikCart.UITests/Helpers/PriceUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TelerikCart.UITests.Core.Helpers;
+
+public static class PriceUtils
+{
+    public static string NormalizePriceString(string price)
+    {
+        return Regex.Replace(price, @"[^0-9.]", "").Trim();
+    }
+
+    public static decimal ParsePrice(string price)
+    {
+        var normalizedPrice = NormalizePriceString(price);
+        if (!decimal.TryParse(normalizedPrice, out var result))
+        {
+            throw new FormatException($"Unable to parse price: {price}");
+        }
+        return result;
+    }
+}
